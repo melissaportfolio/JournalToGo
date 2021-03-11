e1d3148002b88400d9349a7dd707d69ebca7f74e
melissaportfolio <ellieelise@live.com>
2021-03-10 19:29:57 -0700

    .

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static("public"));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+  console.log("Received request for root /");
+
+  res.write("This is the root");
+  res.end();
+});
+
+
+
+
 require('dotenv').config();
+const { Pool } = require('pg');
+
 
 const connectionString = process.env.DATABASE_URL;
 
-// const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({connectionString: connectionString});
+
+var sql = "SELECT * FROM customer";
+
+pool.query(sql, function(err, result) {
+    // If an error occurred...
+    if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+    }
+
+    // Log this to the console for debugging purposes.
+    console.log("Back from DB with result:");
+    console.log(result.rows);
+
+
+}); 
