e55ed6a496a3361ca21f403e6370614a1b2a412c
melissaportfolio <ellieelise@live.com>
2021-03-11 19:26:09 -0700

    .

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+//app listener
+app.set("port", (process.env.PORT || 3000));
+
+app.listen(app.get("port"), function() {
+console.log("Now listening on port: ", app.get("port"));
+});
+
+
+
 app.use(express.static("JournalToGo"));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +35,7 @@ app.get("/", function(req, res) {
 
 // register page
 app.get('/register', function(req, res) {
-    const request = req.query;
+    // const request = req.query;
     res.render('pages/register');
     res.write("This is the registration page");
     res.end();
@@ -42,21 +52,75 @@ const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({connectionString: connectionString,
+ssl: {
+    rejectUnauthorized: false
+}
+});
+
+
+
+
+
+
+
+
+
+//DATABASE CALL from app.get
+app.get("/getCustomers", getCustomers);
+
+function getCustomers(req, res) {
+console.log("Gettting data");
+var id = req.query.id;
+getCustomersFromDataLayer(id, function(error, result) {
+console.log("Back From the getCustomersFromDataLayer:", result);
+if(error || result == null || result.length !=1) {
+res.status(500).json({success:false, data: error});
+}
+else
+{
+res.json(result[0]);
+}
+});
+}
+
+function getCustomersFromDataLayer (id, callback) {
+console.log("getCustomersFromDataLayer called with id", id);
 
 var sql = "SELECT * FROM customer";
+var params = [id];
 
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
+pool.query(sql, params, function(err, result) {
+if (err) {
+console.log("error in database connection");
+console.log(err);
+callback(err, null);
+}
 
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result);
-    // console.log(result.rows);
+console.log("Found DB result:" + JSON.stringify(result.rows));
+
+callback(null, result.rows);
+
+});
+}
 
 
-}); 
+
+
+
+// // var sql = "SELECT * FROM customer";
+
+// pool.query(sql, function(err, result) {
+//     // If an error occurred...
+//     if (err) {
+//         console.log("Error in query: ")
+//         console.log(err);
+//     }
+
+//     // Log this to the console for debugging purposes.
+//     console.log("Back from DB with result:");
+//     console.log(result);
+//     // console.log(result.rows);
+
+
+// }); 
